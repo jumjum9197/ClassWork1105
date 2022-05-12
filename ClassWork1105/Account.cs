d6474e1fbaa9e4833071d7f2b4c969d740bb31a5
Olajumoke Sekoni <sekoniolajumoke@gmail.com>
2022-05-12 09:30:51 +0100

    Add project files.

---
+++ ClassWork1105/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork1105
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public double Amount { get; set; }
+        public char Gender { get; set; }
+
+        public string Yearspecification { get; set; }
+        public int Id { get; set; }
+
+
+    
+  
+    
+        public static List<Account> GetAccounts()
+        {
+            return new List<Account>()
+            {
+                new Account { Name = "Amar", Amount = 400_000, Gender ='f', Id = 2, Yearspecification = "2years" },
+                new Account { Name = "joe", Amount = 40_000, Gender = 'm', Id = 6, Yearspecification = "2months" },
+                new Account { Name = "tola", Amount = 800_000, Gender = 'f', Id = 8, Yearspecification = "5days" },
+                new Account { Name = "jude", Amount = 500_000, Gender ='m', Id = 10, Yearspecification = "6years" },
+                new Account { Name = "amir", Amount = 1000_000, Gender = 'f', Id = 12, Yearspecification = "1years" },
+                new Account { Name = "sam", Amount = 600_000, Gender = 'f', Id = 14, Yearspecification = "1years" },
+                new Account { Name = "joy", Amount = 100_000, Gender = 'f', Id = 16, Yearspecification = "3years" },
+                new Account { Name = "ola", Amount = 20_000, Gender = 'm', Id = 18, Yearspecification = "4months" },
+
+            };
+         
+
+
+        }
+        
+    }
+}
---
+++ ClassWork1105/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ClassWork1105
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account.GetAccounts();
+      
+            var sum = Account.GetAccounts().Sum(x => x.Amount);
+            Task.Run(() => Console.WriteLine($"sum = {sum}"));
+
+            Console.WriteLine();
+
+          
+            var name = Account.GetAccounts().Select(x => x.Name);
+            foreach (var item in name)
+            {
+                Task.Run(() => Console.WriteLine(item));
+                //   Task.Factory.StartNew(() => Console.WriteLine(item));
+            }
+
+
+
+
+         
+   
+          var days = new List<string> { "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday" };
+
+            days.OrderBy(x => days);
+            foreach (var itm in days)
+            {
+                
+                Console.WriteLine(itm);
+            }
+
+
+
+            Console.WriteLine();
+          
+            var month =new List <string> { "jan", "feb", "mar", "apr", "may", "june", "july", "aug", "sept", "oct", "nov", "dec" };
+            var months = month.OrderBy(x => x);
+           
+            foreach (var itm in months)
+            {
+                Console.WriteLine(itm);
+               
+          
+             
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            month.TakeLast(2);
+            var b = months.Reverse().Take(2);
+           var o= b.OrderBy(x => x);
+            foreach (var itm in o)
+            {
+                Console.WriteLine(itm);
+            }
+
+
+
+         
+
+
+
+
+        }
+
+    }
+
+
+    
+  
+   
+    
+      
+      
+
+      
+
+
+    
+    
+
+
+
+
+
+}
